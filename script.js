5d3f06c01cedab7931456be6115a824b9738ae21
ashish-pro <ashishbit017@outlook.com>
2023-09-14 19:28:48 +0530

    pip

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
+// Prompt to selsct media stream, pass to video element, then play
+async function selectMediaStream(){
+    try{
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    }catch(error){
+        // Catch error here
+        console.log('whoops, error here: ', error);
+    }
+}
+
+button.addEventListener('click', async () =>{
+    // Disable Button
+    button.disabled = true;
+    // Start Picture iin Pictute
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+})
+
+//on load
+selectMediaStream();
